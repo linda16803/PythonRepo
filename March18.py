d608a2638ad563fa9a9eb7022167ec24cba25201
lihong <lihong@DESKTOP-KLLFUES>
2019-03-18 17:38:05 -0400

    [20190318] add update to format.

--- March18.py
+++ March18.py
@@ -1,8 +1,4 @@
-def decode(encodeStr):
-    str1 = []
-    for i in range(len(encodeStr)):
-        num = int(encodeStr[i]) if i%2 == 0 else str1.append(num*encodeStr[i])
-    return ("".join(str1))
+
 """
 Run-length encoding is a fast and simple method of encoding strings. 
 The basic idea is to represent repeated successive characters as a single count and character. 
@@ -13,6 +9,13 @@ You can assume the string to be encoded have no digits and consists solely of al
 You can assume the string to be decoded is valid.
 
 """
+
+
+def decode(encodeStr):
+    str1 = []
+    for i in range(len(encodeStr)):
+        num = int(encodeStr[i]) if i%2 == 0 else str1.append(num*encodeStr[i])
+    return ("".join(str1))
 
 def encode(decodeStr):
     str1 = []
